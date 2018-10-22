4cbf82af057907881b458fefd44d7f1bdaf2e635
Micheleen Harris <michhar@microsoft.com>
2018-10-22 15:24:19 -0700

    parameterize label script

--- custom_labeling_classificaiton.py
+++ custom_labeling_classificaiton.py
@@ -1,8 +1,29 @@
+#####################################################
+# Labeling for image classification
+# Run example:
+# python custom_labeling_classificaiton.py --suffix JPG
+#####################################################
+
 import matplotlib.pyplot as plt
 import glob
 import json
+import argparse
 
-all_images = glob.glob('*.JPG')
+def arg_parse():
+    """
+    Parse arguments
+    """
+
+    parser = argparse.ArgumentParser(description='Labeling script for image classification')
+
+
+    parser.add_argument("--suffix", dest='suffix', help="image file ending", type=str)
+
+    return parser.parse_args()
+
+args = arg_parse()
+
+all_images = glob.glob('*.' + args.suffix)
 category={}
 plt.ion()
 
